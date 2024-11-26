d634d42905594a95a7a793f1257050a656727077
gunner <letienhoang@hotmail.com>
2024-11-26 16:49:59 +0700

    update js

--- src/HLS.Topup.Web.Mvc/wwwroot/view-resources/Views/Notification/init.js
+++ src/HLS.Topup.Web.Mvc/wwwroot/view-resources/Views/Notification/init.js
@@ -1,12 +1,12 @@
 ﻿// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBSb_JUP931voxvpcQL0WSdcvP94PbcPsk",
-    authDomain: "nhattrannotification.firebaseapp.com",
-    projectId: "nhattrannotification",
-    storageBucket: "nhattrannotification.appspot.com",
-    messagingSenderId: "5859634411",
-    appId: "1:5859634411:web:3865e980c2cb78b6b3a2ae",
-    measurementId: "G-3G6D4ENS2P"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);

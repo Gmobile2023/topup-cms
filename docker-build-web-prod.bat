c1b8ae063e315badde31940120f75df341cd59f3
gunner <letienhoang@hotmail.com>
2025-07-03 17:06:36 +0700

    update docker

--- docker-build-web-prod.bat
+++ docker-build-web-prod.bat
@@ -1,4 +1,4 @@
-docker build -f src/HLS.Topup.Web.Mvc/Dockerfile -t hls2020/nt:web_v2 .
-docker push hls2020/nt:web_v2
+docker build -f src/HLS.Topup.Web.Mvc/Dockerfile -t hls2020/gmobile:topup_web .
+docker push hls2020/gmobile:topup_web
 
 pause
